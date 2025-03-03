8da555a051a9ccf6d3fef48e0cbf44f4da124037
MouhebSliti <mouheb.sliti@esprit.tn>
2025-03-03 14:16:36 +0100

    a jour

--- services/ratingService.js
+++ services/ratingService.js
@@ -2,7 +2,7 @@ const { getBalance, decreaseBalance } = require('./balanceService');
 
 // Business logic for checking balance and decreasing balance
 const checkAndUpdateBalance = async (companyName, type) => {
-    const cost = type === 'image' ? process.env.IMAGE_COST : type === 'video' ? process.env.VIDEO_COST : 0;
+  const cost = type === 'image' ? process.env.IMAGE_COST : type === 'video' ? process.env.VIDEO_COST : 0;
 
   if (cost === 0) {
     throw new Error('Invalid type');
